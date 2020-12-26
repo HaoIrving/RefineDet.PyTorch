9eabc4acd27a8130c28c42e993bb5cc60549a2bf
HaoIrving <sunwenhao2018@ia.ac.cn>
2020-12-26 09:02:54 -0500

    db

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-python train_refinedet.py --num_workers 16
+python train_refinedet.py --num_workers 16 --save_folder weights/lr_1e3/
+
+git pull origin master
+
+python train_refinedet.py --num_workers 16 --save_folder weights/lr_5e4/
