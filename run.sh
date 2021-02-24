16057d90bf7b36991acca2e849cb4024e58732e7
SUN Wenhao <1825135473@qq.com>
2021-02-23 20:55:28 -0500

    db

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 # python train_refinedet.py --num_workers 16 --lr 1e-3 --save_folder weights/srn_1e3/
 
 
-python train_refinedet.py --num_workers 12 --lr 2e-3 --save_folder weights/align_2e3/ 
+# python train_refinedet.py --num_workers 12 --lr 2e-3 --save_folder weights/align_2e3/ 
+python train_refinedet.py --num_workers 12 --lr 4e-3 --save_folder weights/align_4e3/ 
 # python train_refinedet.py --num_workers 16 --lr 5e-4 --save_folder weights/lr_5e4/
 # python train_refinedet.py --num_workers 16 --lr 3e-3 --save_folder weights/lr_3e3/
 python eval_refinedet_coco.py
