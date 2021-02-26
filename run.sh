628381a78671acc2218d8e4b5baae72454710c94
SUN Wenhao <1825135473@qq.com>
2021-02-26 04:21:56 -0500

    db

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 
 # git pull origin master
-CUDA_VISIBLE_DEVICES=2,3 python train_refinedet.py --num_workers 12 --lr 4e-3 --save_folder weights/at_4e3/ --ngpu 2 --batch_size 16
+# CUDA_VISIBLE_DEVICES=2,3 python train_refinedet.py --num_workers 12 --lr 4e-3 --save_folder weights/at_4e3/ --ngpu 2 --batch_size 16
+python train_refinedet.py --num_workers 12 --lr 2e-3 --save_folder weights/at_2e3/ --batch_size 16
 python eval_refinedet_coco.py
