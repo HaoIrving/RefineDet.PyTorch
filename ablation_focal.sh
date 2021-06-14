61555593a5a030db16e8dff3545a456627e8c65b
SUN Wenhao <1825135473@qq.com>
2021-06-14 10:20:19 +0800

    db

--- ablation_focal.sh
+++ ablation_focal.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=2 python eval_refinedet_coco_ablation.py --prefix weights/a
 
 
 # SANet + deform conv
-python train_refinedet.py --num_workers 12 --lr 4e-3 --save_folder weights/at1_sh_4e3_1_focal_512vggbn/ --ngpu 4 --batch_size 16 --model 512_vggbn 
-CUDA_VISIBLE_DEVICES=2 python eval_refinedet_coco.py --prefix weights/at1_sh_4e3_1_focal_512vggbn --model 512_vggbn 
+python train_refinedet_ablation.py --num_workers 12 --lr 4e-3 --save_folder weights/at1_sh_4e3_1_focal_512vggbn/ --ngpu 4 --batch_size 16 --model 512_vggbn 
+CUDA_VISIBLE_DEVICES=2 python eval_refinedet_coco_ablation.py --prefix weights/at1_sh_4e3_1_focal_512vggbn --model 512_vggbn 
 
 
